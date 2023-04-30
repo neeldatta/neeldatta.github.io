8f4872c76207a3ac7185c6ed1f212f274392e2e3
Neel Datta <62371756+neeldatta@users.noreply.github.com>
2023-04-29 23:17:38 -0700

    hw7 finished

---
+++ homeworks/hw7/assets/js/script.js
@@ -0,0 +1,22 @@
+function averageThreeNumbers(a, b, c) {
+    let sum = a+b+c;
+    let avg = sum/3;
+    return avg
+}
+
+function createSentence(num, noun) {
+    let str = "On average, a Berkeley student has "+ num + noun + "s.";
+    return str
+}
+
+function getRandomNum(max) {
+    return  Math.floor(Math.random() * max);
+}
+
+let x = getRandomNum(20);
+let y = getRandomNum(10);
+let z = getRandomNum(13);
+
+let avg = averageThreeNumbers(x,y,z);
+
+let sentence = createSentence(avg, "dolphin");
